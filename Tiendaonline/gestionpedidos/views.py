6660b16cc3b6d72d95ce04499aa175d2f232dac3
No-Haiku <66711328+No-Haiku@users.noreply.github.com>
2020-12-29 01:16:36 -0300

    tiendaonline.version.001

--- Tiendaonline/gestionpedidos/views.py
+++ Tiendaonline/gestionpedidos/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from gestionpedidos.models import articulos
 
 # Create your views here.
 
@@ -8,7 +9,13 @@ def busqueda_productos(request):
     return render(request,"busqueda_productos.html" )
 
 def buscar(request):
+    if request.GET["product"]:
 
-    mensaje="Articulo buscado %r"%request.GET["product"]
+        #mensaje="Articulo buscado %r"%request.GET["product"]
+        producto=request.GET["product"]
+        artic=articulos.objects.filter(nombre__icontains=producto)
 
+        return render(request,"resultados_busqueda.html",{"articulos":artic,"query":producto})
+    else:
+        mensaje="no has introducido nada"
     return HttpResponse(mensaje)
