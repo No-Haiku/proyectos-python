ebccc3016db9a3c15601577c00539e2c04073ab5
No-Haiku <66711328+No-Haiku@users.noreply.github.com>
2021-01-05 05:11:00 -0300

    actualizacion coneccion con email
    progreso ahora el contacto se conecta con un mal que me envia la request del cliente a otro email

--- Tiendaonline/Tiendaonline/settings.py
+++ Tiendaonline/Tiendaonline/settings.py
@@ -125,3 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#enviar correo
+EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST="smtp.gmail.com"
+EMAIL_USE_TLS=True  
+EMAIL_PORT=587
+EMAIL_HOST_USER="jhonkeee@gmail.com"
+EMAIL_HOST_PASSWORD="digoloquepienso"
--- Tiendaonline/gestionpedidos/views.py
+++ Tiendaonline/gestionpedidos/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from gestionpedidos.models import articulos
+from django.core.mail import send_mail
+from django.conf import settings
 
 # Create your views here.
 
@@ -27,6 +29,17 @@ def buscar(request):
 def contacto(request):
 
     if request.method=="POST":
+
+        subject=request.POST["asunto"]#almacena el asunto
+        
+        message=request.POST["mensaje"] +" "+ request.POST["email"]#almacena el mensaje y email
+        
+        email_from=settings.EMAIL_HOST_USER #de donde viene el email que se envia
+
+        recipient_list=["gian.f-neira@live.com"]#donde quiero que lleguen los msjs
+
+        send_mail(subject, message, email_from, recipient_list)#recordar separar las listas con espacio
+
         return render(request,"gracias.html")
 
     return render(request,"contacto.html")
