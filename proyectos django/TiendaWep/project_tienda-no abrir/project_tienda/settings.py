858690ede327f9e933c60c859af9593f74875af7
No-Haiku <66711328+No-Haiku@users.noreply.github.com>
2021-01-13 04:10:16 -0300

    blog add
    se agrego la funcion de blog

--- proyectos django/TiendaWep/project_tienda-no abrir/project_tienda/settings.py
+++ proyectos django/TiendaWep/project_tienda-no abrir/project_tienda/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ProyectowebApp',
+    'servicios',
 ]
 
 MIDDLEWARE = [
@@ -104,7 +105,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+#LANGUAGE_CODE = 'en-us'}
+LANGUAGE_CODE = 'es-eu'
 
 TIME_ZONE = 'UTC'
 
@@ -119,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL='/media/'
+
+MEDIA_ROOT=BASE_DIR / 'media'
+
